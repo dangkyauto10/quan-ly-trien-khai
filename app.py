f0715b0fc80befcfde1f4753025853dcccb60cc9
dangkyauto10 <dangkyauto10@gmail.com>
2026-09-23 15:39:13 +0700

    Update app.py

--- app.py
+++ app.py
@@ -2,17 +2,16 @@ import streamlit as st
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
-# Kết nối trực tiếp với file credentials.json vừa tải lên GitHub
+# Lấy trực tiếp thông tin từ Secrets của Streamlit
+creds_dict = dict(st.secrets["gcp_service_account"])
 scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
-creds = ServiceAccountCredentials.from_json_keyfile_name("credentials.json", scope)
+
+creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, scope)
 gc = gspread.authorize(creds)
 
-# Mở file Google Sheet và hiển thị dữ liệu
-sheet_name = "QUẢN LÝ DỰ ÁN - HỆ THỐNG ĐIỀU HÀNH"
-sh = gc.open(sheet_name)
-worksheet = sh.get_worksheet(0)
-data = worksheet.get_all_records()
+sh = gc.open("QUẢN LÝ DỰ ÁN - HỆ THỐNG ĐIỀU HÀNH")
+data = sh.get_worksheet(0).get_all_records()
 
 st.title("Quản lý triển khai")
-st.success(f"Đã kết nối thành công tới Google Sheet: {sheet_name}")
+st.success("Đã kết nối thành công tới Google Sheet!")
 st.dataframe(data)
