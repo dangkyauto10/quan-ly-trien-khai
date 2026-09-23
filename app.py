c2203af2327ab5f66a36c8000402c36eecd61c65
dangkyauto10 <dangkyauto10@gmail.com>
2026-09-23 15:47:24 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,10 @@ from streamlit_gsheets import GSheetsConnection
 
 st.title("QUẢN LÝ DỰ ÁN - HỆ THỐNG ĐIỀU HÀNH")
 
-# Kết nối trực tiếp với Google Sheets (chỉ cần link công khai hoặc chia sẻ quyền là đọc được)
+# Kết nối đúng chuẩn với GSheetsConnection
 conn = st.connection("gsheets", type=GSheetsConnection)
-data = conn.read(spreadsheet="https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit") # Thay link sheet của anh vào đây
+
+# Đọc dữ liệu từ đúng link Google Sheet của anh
+data = conn.read(spreadsheet="https://docs.google.com/spreadsheets/d/129gDm3V1Gean0E9jvUXKf3euh7KGieGwzREBFibOoC4/edit?resourcekey=&gid=470200251#gid=470200251")
 
 st.dataframe(data)
